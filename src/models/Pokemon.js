aff7ddcf68ccb33275d293ff0e4e98b908f384bd
Leonardo Ferraz <ldlferraz@gmail.com>
2020-03-02 20:59:03 -0300

    First Commit.

---
+++ src/models/Pokemon.js
@@ -0,0 +1,14 @@
+class Pokemon {
+    constructor() {
+        this.id = -1;
+        this.name = "Undefined Name";
+        this.generation = "Undefined Generation";
+        this.evolutions = ["Undefined Evolutions"];
+        this.types = ["Undefined Type"];
+        this.sprite = "undefinedpokemon.png";
+        this.spriteShiny = "undefinedpokemon.png";
+        this.varieties = ["Undefined Varieties"];
+    }
+}
+
+export default Pokemon;
